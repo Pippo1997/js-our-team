ba568ebf54ba62cffbfc49381241cdb0762dba6b
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-12-13 14:54:55 +0100

    milestone 0

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Array con dentro gli oggetti e le info date
+let team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Carrol",
+        role: "Chief Editor",
+        img: "angela-carrol-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+console.log(team)
